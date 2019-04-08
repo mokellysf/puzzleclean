40775498cf8dc1213be9d5a9391ff333d15869ed
Morgan Kelly <mokelly@Morgans-MacBook-Pro-2.local>
2019-04-08 15:28:41 -0700

    Cleaned out some comments and added pencil toggle for acrostic

--- penciltoggle.js
+++ penciltoggle.js
@@ -1,3 +1,14 @@
+// If crossword pencil then
 var pencil = document.getElementsByClassName("Icon-pencil--1cTxu Icon-icon--1RAWC");
-console.log("pencil is " + pencil[0]);
-pencil[0].click();
+
+if (pencil.length > 0) {
+  console.log("pencil is " + pencil[0]);
+  pencil[0].click();
+}
+
+// If acrostic pencil then
+var ap = document.querySelector('#portal-game-toolbar > div > div:nth-child(3) > button.acrostic-tool.acrostic-tool__icon.acrostic-tool__pencil.pencil-mode-toggle');
+if (ap != null) {
+  console.log("ap pencil found!");
+  ap.click(); 
+}
